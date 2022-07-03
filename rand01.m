6f3326ac0a085e3748ffe87865388cc42c01e6c2
db <boed@cox.net>
2022-07-03 13:51:03 -0700

    bug fix in input argument defaults. Tested.

--- rand01.m
+++ rand01.m
@@ -1,14 +1,10 @@
-function zerosOnes = rand01(p,m,n)
+function zerosOnes = rand01(m,n,p)
 
 % Generates an mxn random matrix of ones (with 
 % probability p) and zeros (with probability 1 - p).
 % Default would be the equivalent of a coin flip with a fair coin.
 
-if nargin < 2
-	n = 1;
-end
-
-if nargin < 1
+if nargin < 3
 	p = 0.5;
 end
 
